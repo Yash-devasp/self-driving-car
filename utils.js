4c8b59fb11767811c9892a35d5cd9d50c80f2251
Yash Goswami <yashgoswami148@gmail.com>
2022-05-17 19:58:04 +0530

    neural network inplemented

--- utils.js
+++ utils.js
@@ -3,11 +3,19 @@ function lerp(A, B, t) {
 }
 
 function getIntersection(A, B, C, D) {
+  if (
+    A === undefined ||
+    B === undefined ||
+    C === undefined ||
+    D === undefined
+  ) {
+    return null;
+  }
   const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
   const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
   const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
 
-  if (bottom !== 0) {
+  if (bottom != 0) {
     const t = tTop / bottom;
     const u = uTop / bottom;
     if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
@@ -18,6 +26,7 @@ function getIntersection(A, B, C, D) {
       };
     }
   }
+
   return null;
 }
 
@@ -36,4 +45,12 @@ function polysIntersect(poly1, poly2) {
     }
   }
   return false;
+}
+
+function getRGBA(value) {
+  const alpha = Math.abs(value);
+  const R = value < 0 ? 0 : 255;
+  const G = R;
+  const B = value > 0 ? 0 : 255;
+  return "rgba(" + R + "," + G + "," + B + "," + alpha + ")";
 }
